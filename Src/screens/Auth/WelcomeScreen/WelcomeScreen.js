44892f5740804bb42ef296ac7adc07a15fb206f3
Murtaza Ahmed <murtazamalik1319@gmail.com>
2025-05-19 17:27:33 +0530

    Structure

---
+++ Src/screens/Auth/WelcomeScreen/WelcomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-gesture-handler";
+
+const WelcomeScreen = () => {
+    return (
+        <View>
+            <Text>Welcome</Text>
+        </View>
+    )
+}
+
+export default WelcomeScreen;
